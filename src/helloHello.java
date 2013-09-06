6a68516ce77efb24c083f0757754d5a15875fb4d
lukylemmie <lemmie6@gmail.com>
2013-09-06 18:25:44 +1000

    hello

---
+++ src/helloHello.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrew2012
+ * Date: 6/09/13
+ * Time: 6:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class helloHello {
+    public static void main(String[] args){
+        System.out.println("HELLO WORLD!");
+    }
+}
